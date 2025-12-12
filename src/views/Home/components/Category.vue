dab2040416ff6d35a0de3a143270591ff86404e5
nidazhong <nidazhong17@163.com>
2025-12-13 03:05:03 +0800

    feat:normal

--- src/views/Home/components/Category.vue
+++ src/views/Home/components/Category.vue
@@ -40,11 +40,13 @@ const { categoryList } = storeToRefs(categoryStore)
   position: relative;
   z-index: 99;
 
-  .menu {
-    li {
+  ul.menu {
+    > li {
       padding-left: 40px;
       height: 55px;
       line-height: 55px;
+      margin: 0 auto;
+      //text-align: center;
 
       &:hover {
         background: $mainColor;
@@ -59,20 +61,21 @@ const { categoryList } = storeToRefs(categoryStore)
         }
       }
 
+      $layer-width:990px;
       .layer {
-        width: 990px;
+        width: $layer-width;
         height: 500px;
         background: rgba(255, 255, 255, 0.8);
         position: absolute;
         left: 250px;
         top: 0;
         display: none;
-        padding: 0 15px;
 
         h4 {
           font-size: 20px;
           font-weight: normal;
-          line-height: 80px;
+          line-height: 60px;
+          padding: 0 15px;
 
           small {
             font-size: 16px;
@@ -83,12 +86,15 @@ const { categoryList } = storeToRefs(categoryStore)
         ul {
           display: flex;
           flex-wrap: wrap;
+          //justify-content: space-evenly;
+          padding: 15px;
+          gap:15px;
 
           li {
-            width: 310px;
+            width: calc(($layer-width - 30px - 30px )/3);
             height: 120px;
-            margin-right: 15px;
-            margin-bottom: 15px;
+            //margin-right: 15px;
+            //margin-bottom: 15px;
             border: 1px solid #eee;
             border-radius: 4px;
             background: #fff;
